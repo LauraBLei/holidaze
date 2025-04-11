fae49fd0cbbd0418f1398d03e333a703df66d600
LauraBLei <laura.blei@outlook.com>
2025-04-11 12:18:13 +0200

    added loading folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Layout } from "./Components/layout";
+import { Layout } from "./components/layout";
 import { HomePage } from "./pages/home";
 import { VenuePage } from "./pages/venue";
 import { ProfilePage } from "./pages/profile";
