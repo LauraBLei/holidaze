3e398f346140af0ac20fbc513fe2aeec5c0f9aa8
LauraBLei <laura.blei@outlook.com>
2025-04-16 15:24:03 +0200

    added styling for button

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ export const HomePage = () => {
       <h1>Home Page</h1>
       <h2>Test commit hook </h2>
       <h2 className="font-primary">Test terminal log when commit</h2>
+      <button className="button transition">Button</button>
     </div>
   );
 };
