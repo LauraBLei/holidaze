aed57dd61e2eee863443ce63b59a78191144bbd4
LauraBLei <laura.blei@outlook.com>
2025-04-16 11:27:21 +0200

    rating component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,14 @@
+import StarRating from '../components/rating';
+
 export const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <h2>Test commit hook </h2>
       <h2>Test terminal log when commit</h2>
+      <div>
+        <StarRating rating={5} />
+      </div>
     </div>
   );
 };
