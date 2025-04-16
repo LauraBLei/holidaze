ceccb755e1fd87089806a58d1411c19c72f2c16c
Haze <117860981+HansiFED@users.noreply.github.com>
2025-04-16 16:10:16 +0200

    Fixed some issues from an earlier merge

--- src/Components/loading/loadingVenueCard.tsx
+++ src/Components/loading/loadingVenueCard.tsx
@@ -1,6 +1,6 @@
 export default function LoadingVenueCard () {
   return (
-    <article className="flex flex-col max-w-[245px] gap-[20px] animate-pulse" aria-hidden="true">
+    <article className="flex flex-col w-[90%] md:max-w-[245px] gap-[20px] animate-pulse">
       <div className="h-[200px] bg-[#C4C4C4] rounded-xl"></div>
       <div className="flex justify-between">
         <div className="bg-[#C4C4C4] h-[27px] w-[60%] rounded"></div>
