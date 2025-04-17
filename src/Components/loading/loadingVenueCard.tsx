c5f0fc75541cfd0b9366461151c274f64c7ea6ce
annechh <142426482+annechh@users.noreply.github.com>
2025-04-17 17:29:06 +0200

    style: made a small adjustment to size

--- src/Components/loading/loadingVenueCard.tsx
+++ src/Components/loading/loadingVenueCard.tsx
@@ -1,6 +1,6 @@
-export default function LoadingVenueCard () {
+export default function LoadingVenueCard() {
   return (
-    <article className="flex flex-col w-[90%] md:max-w-[245px] gap-[20px] animate-pulse">
+    <article className="flex flex-col  md:max-w-[280px] w-full gap-[20px] animate-pulse">
       <div className="h-[200px] bg-[#C4C4C4] rounded-xl"></div>
       <div className="flex justify-between">
         <div className="bg-[#C4C4C4] h-[27px] w-[60%] rounded"></div>
@@ -9,5 +9,5 @@ export default function LoadingVenueCard () {
       <div className="bg-[#C4C4C4] w-full h-[50px] rounded"></div>
       <div className="bg-[#C4C4C4] w-[35%] h-[27px] rounded"></div>
     </article>
-  )
+  );
 }
