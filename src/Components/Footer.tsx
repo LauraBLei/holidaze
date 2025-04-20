6c88de72cf4515ab9700da18859c51f0208934c0
Haze <117860981+HansiFED@users.noreply.github.com>
2025-04-20 17:42:02 +0200

    A rendered footer placed in Layout so it will appear on all pages where inserted

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,13 @@
+/**
+ *
+ * @returns {JSX.element} The rendered foot component
+ */
+
+export default function Footer() {
+  return (
+    <footer className="min-w-screen flex justify-center items-center gap-2 p-10">
+      <img src="/copyright-icon.svg" alt="copyright icon" />
+      <p>HansiFED, LeiDev, HaugeDev</p>
+    </footer>
+  );
+}
