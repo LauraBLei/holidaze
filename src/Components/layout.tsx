ec3bd4163ea777b7aa86f44b40a478542a35b85a
annechh <142426482+annechh@users.noreply.github.com>
2025-04-21 13:26:37 +0200

    Basic header setup for navigation

--- src/Components/layout.tsx
+++ src/Components/layout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer.tsx';
+import Header from './Header.tsx';
 
 export const Layout = () => {
   return (
     <>
-      <header></header>
+      <Header />
       <main className="flex flex-col justify-center items-center w-full">
-        {' '}
         <Outlet />
       </main>
       <Footer />
