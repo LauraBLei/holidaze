6cf77cb40bc4580646ea2493ed9a4fd0dda69bbb
annechh <142426482+annechh@users.noreply.github.com>
2025-04-22 00:07:20 +0200

    Moved nav links to NavList.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,35 +1,38 @@
-import { BiSolidCalendarStar } from 'react-icons/bi';
-import { BsHouseAddFill } from 'react-icons/bs';
-import { IoPerson } from 'react-icons/io5';
+import { BsX } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import logo from '/holidaze-logo.png';
+import { useState } from 'react';
+import { NavList } from './NavList';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 export default function Header() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!isMenuOpen);
+
   return (
-    <header className="w-full">
-      <nav className="flex item-center justify-between mx-5 md:mx-10">
+    <header className="w-full py-5">
+      <nav className="flex item-center justify-between mx-5 md:mx-10 ">
         <Link to="/" className="cursor-pointer">
-          <img src="/holidaze-logo.png" alt="text" />
+          <img src={logo} alt="holidaze-logo" className="h-12 md:h-20" />
         </Link>
-        <ul className="flex gap-6 items-center ">
-          <li className="flex items-center gap-1">
-            <BiSolidCalendarStar className="text-2xl" />
-            <Link to="/manager" className="text-base font-bold cursor-pointer">
-              Manager
-            </Link>
-          </li>
-          <li className="flex items-center gap-1">
-            <BsHouseAddFill className="text-2xl" />
-            <Link to="/create" className="text-base font-bold cursor-pointer">
-              New Venue
-            </Link>
-          </li>
-          <li className="flex items-center gap-1">
-            <IoPerson className="text-2xl" />
-            <Link to="/profile" className="text-base font-bold cursor-pointer">
-              Profile
-            </Link>
-          </li>
-        </ul>
+
+        <div className="hidden md:flex">
+          <NavList className="flex gap-5" />
+        </div>
+
+        <div className="md:hidden self-center">
+          <RxHamburgerMenu className="text-2xl cursor-pointer" onClick={toggleMenu} />
+        </div>
+
+        {isMenuOpen && (
+          <div className="md:hidden absolute top-0 right-0 max-w-96 w-full h-96 bg-[var(--color-brand-orange)] shadow-lg p-5 z-50 flex flex-col items-end rounded-bl-full">
+            <div className="flex pt-2.5">
+              <BsX className="self-end text-3xl mb-5 cursor-pointer" onClick={toggleMenu} />
+            </div>
+            <NavList className=" flex flex-col gap-4 pr-10" onClick={toggleMenu} />
+          </div>
+        )}
       </nav>
     </header>
   );
