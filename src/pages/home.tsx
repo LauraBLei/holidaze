973540afac3b93fa14fb7d7fa4f4d0ba3131f00a
Anne <142426482+annechh@users.noreply.github.com>
2025-04-24 19:17:24 +0200

    Style: wrapped venue cards in grid layout and changed h1 title to All Venues

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,11 +11,13 @@ export const HomePage = () => {
   }, [setVenues]);
 
   return (
-    <>
-      <h1 className="headlineOne">Home Page</h1>
-      {venues.map((venue) => {
-        return <VenueCard venue={venue} key={venue.id} />;
-      })}
-    </>
+    <section className="mx-5">
+      <h1 className="headlineOne">All Venues</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10">
+        {venues.map((venue) => {
+          return <VenueCard venue={venue} key={venue.id} />;
+        })}
+      </div>
+    </section>
   );
 };
