90ccdac157ca871e4b679150a22bcd5247919383
LauraBLei <laura.blei@outlook.com>
2025-04-28 15:51:09 +0200

    made form for rest of register

--- src/Components/register.tsx
+++ src/Components/register.tsx
@@ -6,34 +6,88 @@ export const RegisterModal = () => {
   const { registerOpen, OpenLogin } = useContext(CommonContext);
   return (
     <>
-      <section className={`absolute z-50 ${registerOpen ? 'block' : 'hidden'}`}>
-        <div>
-          <h1>Register an account</h1>
-          <form action="">
-            <div>
-              <button
-                type="button"
-                className={`flex-1 text-center z-10 font-medium py-2 ${
-                  !isVenueManager ? 'text-black' : 'text-gray-500'
-                }`}
-                onClick={() => setIsVenueManager(false)}
-              >
-                Customer
-              </button>
-              <button
-                type="button"
-                className={`flex-1 text-center z-10 font-medium py-2 ${
-                  !isVenueManager ? 'text-black' : 'text-gray-500'
-                }`}
-                onClick={() => setIsVenueManager(true)}
-              >
-                Venue Manager
-              </button>
+      <section
+        className={`absolute z-50 bg-black/50 h-screen w-full flex items-center justify-center ${registerOpen ? 'block' : 'hidden'}`}
+      >
+        <div className="bg-white w-full md:max-w-[750px] max-h-[750px] h-full flex flex-col justify-center items-center rounded-xl">
+          <h1 className="">Register an account</h1>
+          <form className="w-full flex flex-col gap-4 max-w-[425px] items-center" action="">
+            <div id="Slider" className="w-full  mx-auto">
+              <input type="hidden" name="isVenueManager" value={isVenueManager.toString()} />
+
+              <div className="relative flex rounded-full p-1  w-full">
+                <div
+                  id="movingSlider"
+                  className={`absolute top-1 left-1 w-1/2 h-full rounded-xl border-brand-grey border-2 bg-white shadow-md transition-transform duration-300 ${
+                    isVenueManager ? 'translate-x-full' : ''
+                  }`}
+                />
+
+                <button
+                  type="button"
+                  onClick={() => setIsVenueManager(false)}
+                  className={`flex-1 z-10 py-3.5 px-14 font-medium text-xl  ${
+                    !isVenueManager
+                      ? 'text-black cursor-auto'
+                      : 'text-brand-grey whitespace-nowrap cursor-pointer'
+                  }`}
+                >
+                  Customer
+                </button>
+
+                {/* Venue Manager button */}
+                <button
+                  type="button"
+                  onClick={() => setIsVenueManager(true)}
+                  className={`flex-1 z-10 py-3.5 px-14 font-medium text-xl text-black whitespace-nowrap ${
+                    isVenueManager
+                      ? 'text-black cursor-auto'
+                      : 'text-brand-grey whitespace-nowrap cursor-pointer'
+                  } `}
+                >
+                  Venue Manager
+                </button>
+              </div>
             </div>
+            <div id="error messages" className="hidden">
+              <span>Register Successful</span>
+            </div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              required
+              className="w-full p-3 rounded-xl border border-brand-grey  "
+            />
+
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+              className="w-full p-3 rounded-xl border border-brand-grey "
+            />
+
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+              className="w-full p-3 rounded-xl border border-brand-grey "
+            />
+
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              required
+              className="w-full p-3 rounded-xl border border-brand-grey "
+            />
+
+            <button type="submit" className="button transition ">
+              Register
+            </button>
           </form>
-          <div id="error messages" className="hidden">
-            Error
-          </div>
         </div>
         <button onClick={() => OpenLogin()}>Login</button>
       </section>
