faeebeb00ae9f8013abe922547a4ccc7fdc96df7
annechh <142426482+annechh@users.noreply.github.com>
2025-04-29 16:55:28 +0200

    Import useParams and logs id on venue page

--- src/pages/venue.tsx
+++ src/pages/venue.tsx
@@ -1,9 +1,14 @@
 import LoadingVenuePage from '../Components/loading/loadingVenuePage';
 
+import { useParams } from 'react-router-dom';
+
 export const VenuePage = () => {
+  const { id } = useParams();
+  console.log('id', id);
+
   return (
-    <>
+    <div>
       <LoadingVenuePage />
-    </>
+    </div>
   );
 };
