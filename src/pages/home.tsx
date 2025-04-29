c2eecdf0c33bd7a1fe3303c2ba6185a9446fd3d6
annechh <142426482+annechh@users.noreply.github.com>
2025-04-29 17:00:19 +0200

    style: update all venues grid for lg scrren to 4, and md screen to 3

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -27,7 +27,7 @@ export const HomePage = () => {
     <section className="mx-5">
       <Search id="search" onSearch={handleSearch} />
       <h1 className="headlineOne">All Venues</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-10">
         {venues.map((venue) => {
           return <VenueCard venue={venue} key={venue.id} />;
         })}
