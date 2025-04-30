ebb0658f10bcecf3fc78c72e87ee521cfdbce7fe
annechh <142426482+annechh@users.noreply.github.com>
2025-04-30 18:32:59 +0200

    Style: search bar no longer gets small when search has no result

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -23,17 +23,19 @@ export const HomePage = () => {
   };
 
   return (
-    <section className="mx-5 max-w-[1240px] w-full">
-      <Search id="search" onSearch={handleSearch} />
-      {venues.length === 0 && (
-        <p className="text-center text-gray-500">No venues found matching your search.</p>
-      )}
-      <h1 className="headlineOne">All Venues</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-10">
-        {venues.map((venue) => {
-          return <VenueCard venue={venue} key={venue.id} />;
-        })}
-      </div>
-    </section>
+    <div className="mx-5 w-full flex flex-col items-center justify">
+      <section className="max-w-[1240px] w-full">
+        <Search id="search" onSearch={handleSearch} />
+        {venues.length === 0 && (
+          <p className="text-center text-gray-500">No venues found matching your search.</p>
+        )}
+        <h1 className="headlineOne mb-5 md:mb-8">All Venues</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-10">
+          {venues.map((venue) => {
+            return <VenueCard venue={venue} key={venue.id} />;
+          })}
+        </div>
+      </section>
+    </div>
   );
 };
