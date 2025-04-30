3c819b716437e324630351f86d73232308f369e5
annechh <142426482+annechh@users.noreply.github.com>
2025-04-30 18:33:32 +0200

    Style: add min-h-screen and flex-grow in layout.tsx

--- src/Components/layout.tsx
+++ src/Components/layout.tsx
@@ -6,14 +6,14 @@ import { RegisterModal } from './register.tsx';
 
 export const Layout = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex flex-col justify-center items-center w-full">
+      <main className="flex flex-col items-center w-full flex-grow">
         <Outlet />
         <LoginModal />
         <RegisterModal />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
