efd19d1ceb2cf964269661196fec2af7b7bb408c
annechh <142426482+annechh@users.noreply.github.com>
2025-04-30 18:04:55 +0200

    add: conditinally render a message if no venues are found

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -23,8 +23,11 @@ export const HomePage = () => {
   };
 
   return (
-    <section className="mx-5">
+    <section className="mx-5 max-w-[1240px] w-full">
       <Search id="search" onSearch={handleSearch} />
+      {venues.length === 0 && (
+        <p className="text-center text-gray-500">No venues found matching your search.</p>
+      )}
       <h1 className="headlineOne">All Venues</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-10">
         {venues.map((venue) => {
