2de970fb8c6288a3030e3f176976511f94aee1e1
annechh <142426482+annechh@users.noreply.github.com>
2025-05-01 16:49:08 +0200

    Add amenities to the create new venue form

--- src/Components/CreateVenue.tsx
+++ src/Components/CreateVenue.tsx
@@ -1,4 +1,6 @@
-import { PiPlusCircle } from 'react-icons/pi';
+import { FaDog, FaParking } from 'react-icons/fa';
+import { IoCheckmarkSharp } from 'react-icons/io5';
+import { PiForkKnifeBold, PiPlusCircle } from 'react-icons/pi';
 
 const title = 'text-3xl pb-2 border-b border-brand-grey mb-8';
 
@@ -6,13 +8,14 @@ export const CreateVenueForm = () => {
   return (
     <div className="max-w-[1000px] w-full h-full flex flex-col justify-center">
       <form className="mx-5 flex flex-col gap-28">
+        {/* Basic info */}
         <div className="flex flex-col gap-4">
           <h2 className={title}>Basic info</h2>
           <div className="w-full">
             <label htmlFor="image" className="text-base">
               URL - Media
             </label>
-            <div className="relative">
+            <div className="relative mt-1">
               <input
                 id="image"
                 type="url"
@@ -29,7 +32,7 @@ export const CreateVenueForm = () => {
             <label htmlFor="name" className="text-base">
               Name of venue
             </label>
-            <div className="relative">
+            <div className="relative pt-1">
               <input id="name" type="text" className="input" placeholder="Name of the venue" />
             </div>
           </div>
@@ -38,7 +41,7 @@ export const CreateVenueForm = () => {
             <label htmlFor="description" className="text-base">
               Description
             </label>
-            <div className="relative">
+            <div className="relative pt-1">
               <textarea
                 id="description"
                 className=" w-full h-36 border border-brand-grey p-3 rounded-xl "
@@ -48,6 +51,7 @@ export const CreateVenueForm = () => {
           </div>
         </div>
 
+        {/* Pricing & Guests */}
         <div className="flex flex-col gap-4">
           <h2 className={title}>Pricing & Guests</h2>
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
@@ -55,7 +59,7 @@ export const CreateVenueForm = () => {
               <label htmlFor="price" className="text-base">
                 Price
               </label>
-              <div className="relative">
+              <div className="relative pt-1">
                 <input
                   id="price"
                   type="number"
@@ -68,7 +72,7 @@ export const CreateVenueForm = () => {
               <label htmlFor="max-guests" className="text-base">
                 Max Guests
               </label>
-              <div className="relative">
+              <div className="relative pt-1">
                 <input
                   id="max-guests"
                   type="number"
@@ -84,7 +88,7 @@ export const CreateVenueForm = () => {
               <label htmlFor="rating" className="text-base">
                 Rating
               </label>
-              <div className="relative">
+              <div className="relative pt-1">
                 <input
                   id="rating"
                   type="number"
@@ -99,6 +103,7 @@ export const CreateVenueForm = () => {
           </div>
         </div>
 
+        {/* Location */}
         <div className="flex flex-col gap-4">
           <h2 className={title}>Location</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
@@ -106,33 +111,120 @@ export const CreateVenueForm = () => {
               <label htmlFor="address" className="text-base">
                 Address
               </label>
-              <div className="relative">
-                <input id="address" type="text" className="input" placeholder="Placeholder text" />
+              <div className="relative pt-1">
+                <input
+                  id="address"
+                  type="text"
+                  className="input"
+                  placeholder="Imaginary street 123"
+                />
               </div>
             </div>
             <div className="w-full">
               <label htmlFor="city" className="text-base">
                 City
               </label>
-              <div className="relative">
-                <input id="city" type="text" className="input" placeholder="Placeholder text" />
+              <div className="relative pt-1">
+                <input id="city" type="text" className="input" placeholder="Bergen" />
               </div>
             </div>
             <div className="w-full">
               <label htmlFor="zip-code" className="text-base">
                 Zip-Code
               </label>
-              <div className="relative">
-                <input id="zip-code" type="text" className="input" placeholder="Placeholder text" />
+              <div className="relative pt-1">
+                <input id="zip-code" type="text" className="input" placeholder="1010" />
               </div>
             </div>
             <div className="w-full">
               <label htmlFor="country" className="text-base">
                 Country
               </label>
-              <div className="relative">
-                <input id="country" type="text" className="input" placeholder="Placeholder text" />
+              <div className="relative pt-1">
+                <input id="country" type="text" className="input" placeholder="Dreamland" />
               </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Amenities */}
+        <div className="flex flex-col gap-4">
+          <h2 className={title}>Amenities</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+            <div className="flex w-full p-2 rounded bg-red-300">
+              <label
+                htmlFor="pets"
+                className="flex justify-between items-center w-full cursor-pointer"
+              >
+                <div className="flex items-center gap-2">
+                  <FaDog size={24} />
+                  <span className="text-base">Pets</span>
+                </div>
+
+                <div className="relative">
+                  <input type="checkbox" id="pets" className="sr-only peer" />
+                  <span className="w-6 h-6 border-1 bg-white border-black rounded-md flex items-center justify-center peer-checked:bg-black">
+                    <IoCheckmarkSharp size={18} className="text-white hidden peer-checked:block" />
+                  </span>
+                </div>
+              </label>
+            </div>
+
+            <div className="flex w-full p-2 rounded bg-blue-300">
+              <label
+                htmlFor="parking"
+                className="flex justify-between items-center w-full cursor-pointer"
+              >
+                <div className="flex items-center gap-2">
+                  <FaParking size={24} />
+                  <span className="text-base">Parking</span>
+                </div>
+
+                <div className="relative">
+                  <input type="checkbox" id="parking" className="sr-only peer" />
+                  <span className="w-6 h-6 border-1 bg-white border-black rounded-md flex items-center justify-center peer-checked:bg-black">
+                    <IoCheckmarkSharp size={18} className="text-white hidden peer-checked:block" />
+                  </span>
+                </div>
+              </label>
+            </div>
+
+            <div className="flex w-full p-2 rounded bg-purple-300">
+              <label
+                htmlFor="breakfast"
+                className="flex justify-between items-center w-full cursor-pointer"
+              >
+                <div className="flex items-center gap-2">
+                  <PiForkKnifeBold size={24} />
+                  <span className="text-base">Breakfast</span>
+                </div>
+
+                <div className="relative">
+                  <input type="checkbox" id="breakfast" className="sr-only peer" />
+                  <span className="w-6 h-6 border-1 bg-white border-black rounded-md flex items-center justify-center peer-checked:bg-black">
+                    <IoCheckmarkSharp size={18} className="text-white hidden peer-checked:block" />
+                  </span>
+                </div>
+              </label>
+            </div>
+
+            <div className="flex w-full p-2 rounded bg-orange-300">
+              <label
+                htmlFor="wifi"
+                className="flex justify-between items-center w-full cursor-pointer"
+              >
+                <div className="flex items-center gap-2">
+                  <FaParking size={24} />
+                  <span className="text-base">Free Wifi</span>
+                </div>
+
+                <div className="relative">
+                  <input type="checkbox" id="wifi" className="sr-only peer" />
+                  <span className="w-6 h-6 border-1 bg-white border-black rounded-md flex items-center justify-center peer-checked:bg-black">
+                    <IoCheckmarkSharp size={18} className="text-white hidden peer-checked:block" />
+                  </span>
+                </div>
+              </label>
             </div>
           </div>
         </div>
