aea7035f088ecd6563c612c801bf9fa931253b9f
annechh <142426482+annechh@users.noreply.github.com>
2025-05-03 16:55:30 +0200

    style: add classname to title

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -3,7 +3,7 @@ import { CreateVenueForm } from '../Components/CreateVenue';
 export const CreatePage = () => {
   return (
     <section className="w-full flex flex-col items-center justify-center">
-      <h1>Create your new Venue</h1>
+      <h1 className="headlineOne mt-7 md:mt-16">Create your new Venue</h1>
       <CreateVenueForm />
     </section>
   );
