e9b462929c3f885b8e4064418c9db4e0526f1227
annechh <142426482+annechh@users.noreply.github.com>
2025-05-03 04:05:58 +0200

    chore: add media to handleCreateVenueSubmit

--- src/UI/venue/create.tsx
+++ src/UI/venue/create.tsx
@@ -1,21 +1,22 @@
 import { HandleCreateVenue } from '../../API/venues/create';
+import { Media } from '../../Types/common';
 
-export const handleCreateVenueSubmit = (formdata: FormData) => {
+export const handleCreateVenueSubmit = (formdata: FormData, media: Media[]) => {
   const name = formdata.get('name');
   const description = formdata.get('description');
   const price = formdata.get('price');
   const maxGuests = formdata.get('maxGuests');
 
-  console.log('📦 Venue form data:', { name, description, price, maxGuests });
+  console.log('📦 Venue form data:', { media, name, description, price, maxGuests });
 
   try {
     HandleCreateVenue({
+      media,
       name: name?.toString() || '',
       description: description?.toString() || '',
       price: Number(price),
       maxGuests: Number(maxGuests),
     });
-    console.log('🌛 Venue form data:', { name, description, price, maxGuests });
   } catch (error) {
     console.error('Venue creation failed:', error);
   }
