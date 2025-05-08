dc2b51612c0f8118d8ede5e321f778c3e1159c5c
annechh <142426482+annechh@users.noreply.github.com>
2025-05-08 15:36:34 +0200

    add reload page when loggin in

--- src/API/auth/login.ts
+++ src/API/auth/login.ts
@@ -22,6 +22,7 @@ export default async function HandleLogin(formdata: FormData) {
 
     if (response.ok) {
       localStorage.setItem('User', JSON.stringify(data.data));
+      window.location.reload();
     }
 
     return data.data;
