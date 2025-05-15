dca9b875922ce1f165f90877b7de93625d2a73bc
annechh <142426482+annechh@users.noreply.github.com>
2025-05-15 02:57:12 +0200

    refactor: moved validation handling to a seperate file

--- src/UI/auth/register.tsx
+++ src/UI/auth/register.tsx
@@ -1,31 +1,12 @@
-import { HandleRegister } from '../../API/auth/register';
+import { fetchRegister } from '../../API/auth/register';
 
-export const handleRegisterSubmit = (formdata: FormData) => {
-  const password = formdata.get('password');
-  const confirmPassword = formdata.get('confirmPassword');
+export const handleRegisterSubmit = async (formdata: FormData) => {
   const name = formdata.get('name');
   const email = formdata.get('email');
+  const password = formdata.get('password');
   const isVenueManager = formdata.get('isVenueManager') === `true`;
-  const emailPattern = /^[\w\-.]+@(stud\.)?noroff\.no$/;
-
-  const emailString = email?.toString() || '';
-  if (!emailPattern.test(emailString)) {
-    document.getElementById('wrongEmailPattern')?.classList.remove('hidden');
-    setTimeout(() => {
-      document.getElementById('wrongEmailPattern')?.classList.add('hidden');
-    }, 5000);
-    return;
-  }
-
-  if (password !== confirmPassword) {
-    document.getElementById('passwordError')?.classList.remove('hidden');
-    setTimeout(() => {
-      document.getElementById('passwordError')?.classList.add('hidden');
-    }, 5000);
-    return;
-  }
 
   if (name && email && password) {
-    HandleRegister({ name, email, password, isVenueManager });
+    await fetchRegister({ name, email, password, isVenueManager });
   }
 };
