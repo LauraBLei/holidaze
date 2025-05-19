525f2f35682a3521561bd62649001d8dd5f2fcc9
annechh <142426482+annechh@users.noreply.github.com>
2025-05-19 12:00:49 +0200

    fix: unconsistensy with filenames using lower and uppercase for component files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { Layout } from './Components/TempLayout';
+import { Layout } from './Components/layout';
 import { HomePage } from './pages/home';
 import { VenuePage } from './pages/venue';
 import { ProfilePage } from './pages/profile';
