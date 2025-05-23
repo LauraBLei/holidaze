8fdceba02fefc2422566e05f7ffe8b7c62656d90
annechh <142426482+annechh@users.noreply.github.com>
2025-05-23 13:01:07 +0200

    added banner to skeleton loader home

--- src/Components/loading/SkeletonLoaderHome.tsx
+++ src/Components/loading/SkeletonLoaderHome.tsx
@@ -17,10 +17,13 @@ export default function SkeletonLoaderHome() {
     <div className="w-full flex flex-col items-center justify">
       <div className="max-w-[1440px] w-full font primary">
         <div className="mx-5 2xl:mx-0">
+          <div className="max-w-[1440px] w-full h-[600px] overflow-hidden mb-10">
+            <div className="bg-brand-grey w-full h-full object-cover"></div>
+          </div>
           <div role="search" className="pt-10 mb-16 w-full flex justify-center items-center">
             <div className="max-w-[650px] w-full relative animate-pulse">
-              <div className="w-full h-12 bg-gray-200 rounded"></div>
-              <div className="w-12 h-12 bg-gray-300 rounded absolute top-0 right-0"></div>
+              <div className="w-full h-12 bg-gray-200 rounded-xl"></div>
+              <div className="w-12 h-12 bg-gray-300 rounded-xl absolute top-0 right-0"></div>
             </div>
           </div>
           <section className="max-w-[1440px] ">
@@ -40,6 +43,13 @@ export default function SkeletonLoaderHome() {
               <SkeletonLoaderVenueCard />
               <SkeletonLoaderVenueCard />
               <SkeletonLoaderVenueCard />
+            </div>
+            <div className="flex justify-center items-center gap-4 mt-10 animate-pulse">
+              <div className="h-10 max-w-[100px] text-sm md:text-base w-full bg-brand-grey rounded disabled:opacity-50 cursor-pointer scale-95 hover:scale-100 transition"></div>
+
+              <span className="bg-brand-grey w-16 h-5 rounded"></span>
+
+              <div className="h-10 max-w-[100px] text-sm md:text-base w-full bg-brand-grey rounded disabled:opacity-50 cursor-pointer scale-95 hover:scale-100 transition"></div>
             </div>
           </section>
         </div>
