faf1a4e82bd9f9c71b9fefd57bf854511e07eafa
Anne <142426482+annechh@users.noreply.github.com>
2025-05-24 18:47:48 +0200

    optimized banner and error image

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 
 export const ErrorPage = () => {
   return (
-    <>
+    <div className="w-full flex flex-col items-center">
       <h1 className="opacity-0">Error 404</h1>
-      <img src="./error.png" alt="Error 404 Image" />
-      <Link className="button text-center mt-2" to="/">
+      <div className="max-w-[800px] w-full">
+        <img src="./error.png" alt="Error 404 Image" className="" />
+      </div>
+      <Link className="button text-center mt-5" to="/">
         Go back to home!
       </Link>
-    </>
+    </div>
   );
 };
